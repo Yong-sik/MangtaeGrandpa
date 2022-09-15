26539ddd04df792d630f66410d196612c0342702
Yong-sik <om77901@gmail.com>
2022-09-16 02:03:03 +0900

    [09.16] colors.xml에 색상 추가 및 버튼 모양 drawble 추가

--- app/src/main/java/com/grandpa/mangtae/AddressBookActivity.java
+++ app/src/main/java/com/grandpa/mangtae/AddressBookActivity.java
@@ -10,5 +10,6 @@ public class AddressBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_book);
+
     }
 }
